dcd4ee832cd62e7f8f72af9ecf23193b40483088
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-24 12:17:29 +0100

    remove unused elements from app

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,6 @@ import './App.css';
 import {Counter} from "./components/state/Counter";
 
 function App() {
-  const personName = {
-    first: "Bruce",
-    last: "Wayne"
-  }
-
   return (
     <div className="App">
       <Counter></Counter>
