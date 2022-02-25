6392abc32ad4d29d38aa33a9288bf73adb1f1644
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-25 15:35:14 +0100

    add class component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import './App.css';
-import { UserContextProvider } from './components/context/UserContext';
-import { User } from './components/context/User';
+import { Counter } from './components/class/Counter';
 function App() {
   return (
     <div className="App">
-      <UserContextProvider>
-        <User></User>
-      </UserContextProvider>
+     <Counter message='The count value is'></Counter>
     </div>
   );
 } 
