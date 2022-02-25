ca36a358ffc019bdc7f0ba195bab4c83017c3108
Gaiseric <sebastiaancrisan@gmail.com>
2022-02-25 15:03:48 +0100

    add usercontext api

---
+++ src/components/context/User.tsx
@@ -0,0 +1,22 @@
+import { useContext } from "react"
+import { UserContext } from "./UserContext"
+export const User = () => {
+    const userContext = useContext(UserContext);
+    const handleLogin = () => {
+        userContext.setUser({
+            name: "Gaiseric",
+            email: "Gaiseric@mail.com"
+        }); 
+    }
+    const handleLogout = () => {
+        userContext.setUser(null);
+    }
+    return (
+        <div>
+            <button onClick={handleLogin}>Login</button>
+            <button onClick={handleLogout}>Logout</button>
+            <div>User name is {userContext.user?.name}</div>
+            <div>User mail is {userContext.user?.email}</div>
+        </div>
+    )
+}
---
+++ src/components/context/UserContext.tsx
@@ -0,0 +1,18 @@
+import { createContext, useState } from "react"
+
+type UserContextProviderProps = {
+    children: React.ReactNode;
+}
+type UserContextType = {
+    user: AuthUser | null;
+    setUser: React.Dispatch<React.SetStateAction<AuthUser | null >>
+}
+export const UserContext = createContext({} as UserContextType);
+export type AuthUser = {
+    name:string;
+    email:string;
+}
+export const UserContextProvider = ({children}:UserContextProviderProps) => {
+    const [user, setUser] = useState<AuthUser|null>(null);
+    return <UserContext.Provider value={{user, setUser}}>{children}</UserContext.Provider>
+}
